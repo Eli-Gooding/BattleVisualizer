3ff406f1fd95f35f062aef78ea45c9c8b818d9a5
Eli-Gooding <eli.w.gooding@gmail.com>
2025-02-22 17:57:19 -0600

    added analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { BattleProvider } from './contexts/BattleContext';
 import App from './App';
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <BattleProvider>
       <App />
+      <Analytics />
     </BattleProvider>
   );
 } 
